81d99e6419277db658ebc77b5c704564c4f8bd6e
diego7alan <diego7alan@gmail.com>
2019-03-28 14:37:32 -0600

    PRUEBA 3 - AGREGO ALGO DEL ROOT

---
+++ app/src/main/java/com/example/juankno4/simsaticket/cRoot/cRoot.java
@@ -0,0 +1,16 @@
+package com.example.juankno4.simsaticket.cRoot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.juankno4.simsaticket.R;
+
+public class cRoot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_root_inicio);
+    }
+}
