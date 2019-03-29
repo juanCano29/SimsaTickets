4b79efc295d1586c52973f39bba0449f371be7fc
kiikedls <jespinozadelossatnos@gmail.com>
2019-03-29 12:25:02 -0600

    agrege un menu drawer y un fragment con cards para el historial

--- app/src/main/java/com/example/juankno4/simsaticket/MainActivity.java
+++ app/src/main/java/com/example/juankno4/simsaticket/MainActivity.java
@@ -6,22 +6,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.juankno4.simsaticket.cEmp.activity_empleado_inicio;
 import com.example.juankno4.simsaticket.cTec.TecnicoInicioActivity;
 
 public class MainActivity extends AppCompatActivity {
- Button btn2;
+ Button btn2,btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btn2 = findViewById(R.id.btn2);
+        btn1=findViewById(R.id.btn1);
 
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent li = new Intent(MainActivity.this,TecnicoInicioActivity.class);
                 startActivity(li);
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent x=new Intent(MainActivity.this, activity_empleado_inicio.class);
+                startActivity(x);
             }
         });
 
